afd178b73bc2ac919a1d7762aba44936561b5efb
Thiago Puntar <thiagopuntar@gmail.com>
2025-03-05 12:15:17 -0300

    use same clipboard for neovim and os

--- .config/nvim/lua/puntart/remap.lua
+++ .config/nvim/lua/puntart/remap.lua
@@ -5,10 +5,6 @@ vim.keymap.set("n", "<leader>h", ":noh<CR>")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
--- Copy and paste to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
-
 -- move block as Vs code alt arrow down / arrow up
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- .config/nvim/lua/puntart/set.lua
+++ .config/nvim/lua/puntart/set.lua
@@ -6,6 +6,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.cursorline = true
+vim.opt.clipboard = 'unnamedplus'
 
 -- Better view on smaller screens
 vim.opt.breakindent = true
