3e525b8f1c7c272df5a3e1eb1939c5e0d9316c12
Thiago Puntar <thiagopuntar@gmail.com>
2025-05-15 04:33:30 -0300

    fix new monitor on i3

--- .config/i3/i3_startup.sh
+++ .config/i3/i3_startup.sh
@@ -4,7 +4,7 @@ xinput set-prop "UNIW0001:00 093A:0255 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "UNIW0001:00 093A:0255 Touchpad" "libinput Natural Scrolling Enabled" 1
 
 # Setup external monitors
-xrandr --output HDMI-1-0 --auto --right-of eDP-1
+xrandr --output DP-1-0 --auto --right-of eDP-1
 
 
 
